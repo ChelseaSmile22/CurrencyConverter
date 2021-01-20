aac1a4badc7e75ab18add9571d0e0a45dcd8ed7f
Ultraviolence228 <diadelosmuertos95@gmail.com>
2021-01-20 21:20:27 +0400

    for tinkoff

---
+++ CurrencyConverter/Currencies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyConverter
+{
+    public static class Currencies
+    {
+        public const double USD = 1;
+        public const double RUB = 0.014;
+        public const double EUR = 1.21;
+        public const double SHEKEL = 0.316;
+        public const double NIGER = 0.026;
+    }
+}
---
+++ CurrencyConverter/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CurrencyConverter
+{
+    class Program
+    {
+        private static double amount = 0;
+        private static string currencyFrom = "";
+        private static string currencyTo = "";
+
+
+        private static string Enter()
+        {
+            Console.WriteLine("Enter amount and currency as \"10 usd to rub\" :  ");
+            return Console.ReadLine();
+        }
+
+        private static void Parse(string input)
+        {
+            var InputArray = input.Split(' ');
+            amount = Convert.ToDouble(InputArray[0]);
+            currencyFrom = InputArray[1];
+            currencyTo = InputArray[3];
+        }
+
+        private static double CalculateToUsd()
+        {
+            switch (currencyFrom)
+            {
+                case "usd": return amount;
+                case "eur": return amount * Currencies.EUR;
+                case "rub": return amount * Currencies.RUB;
+                case "nigger": return amount * Currencies.NIGER;
+                case "kike": return amount * Currencies.SHEKEL;
+                default: 
+                    Console.WriteLine("Введите правильную валюту");
+                    return -1;
+            }
+        }
+
+        private static double CalculateResult(double input)
+        {
+            switch (currencyTo)
+            {
+                case "usd": return input;
+                case "eur": return input / Currencies.EUR;
+                case "rub": return input / Currencies.RUB;
+                case "nigger": return input / Currencies.NIGER;
+                case "kike": return input / Currencies.SHEKEL;
+                default:
+                    Console.WriteLine("Введите правильную валюту");
+                    return -1;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Parse(Enter());
+                Console.WriteLine($"Выходит, что: { CalculateResult(CalculateToUsd())}");
+            }
+            
+        }
+    }
+}
